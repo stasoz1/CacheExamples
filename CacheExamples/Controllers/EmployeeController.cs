32459346e50d65df3eb4587a3eceac46e8c8d70b
stasoz <stasozgithub@gmail.com>
2021-12-09 17:22:24 +0200

    init

---
+++ CacheExamples/Controllers/EmployeeController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CacheExamples.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IDistributedCache _distributedCache;
+        private readonly string employeesCollectionKey = "employeesCollectionKey";
+
+        public EmployeeController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+        public async Task<IEnumerable<Employee>> Get()
+        {
+            // Find cached item
+            byte[] objectFromCache = await _distributedCache.GetAsync(employeesCollectionKey);
+
+            if (objectFromCache != null)
+            {
+                // Deserialize it
+                var jsonToDeserialize = System.Text.Encoding.UTF8.GetString(objectFromCache);
+                var cachedResult = JsonSerializer.Deserialize<IEnumerable<Employee>>(jsonToDeserialize);
+                if (cachedResult != null)
+                {
+                    // If found, then return it
+                    return cachedResult;
+                }
+            }
+
+            // If not found, then recalculate response
+            var result = GetEmployees();
+
+            // Serialize the response
+            byte[] objectToCache = JsonSerializer.SerializeToUtf8Bytes(result);
+            var cacheEntryOptions = new DistributedCacheEntryOptions()
+                .SetSlidingExpiration(TimeSpan.FromSeconds(5))
+                .SetAbsoluteExpiration(TimeSpan.FromSeconds(10));
+
+            // Cache it
+            await _distributedCache.SetAsync(employeesCollectionKey, objectToCache, cacheEntryOptions);
+
+            return result;
+        }
+
+        private static IEnumerable<Employee> GetEmployees()
+        {
+            return new List<Employee>()
+            {
+                 new Employee() { Id = 1, Name = "John", Position = "Back-End Develop" },
+                 new Employee() { Id = 2, Name = "Daniel", Position = "Front-End Developer" },
+                 new Employee() { Id = 3, Name = "Steve", Position = "Team Lead" },
+            };
+        }
+
+        public class Employee
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Position { get; set; }
+        }
+    }
+}
---
+++ CacheExamples/Controllers/WeatherForecastController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CacheExamples.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IMemoryCache _memoryCache;
+        private readonly string weatherForecastKey = "weatherForecastKey";
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IMemoryCache memoryCache)
+        {
+            _logger = logger;
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            IEnumerable<WeatherForecast> weatherForecastCollection = null;
+
+            // If found in cache, return cached data
+            if (_memoryCache.TryGetValue(weatherForecastKey, out weatherForecastCollection))
+            {
+                return weatherForecastCollection;
+            }
+
+            // If not found, then calculate response
+            weatherForecastCollection = GetWeatherForecast();
+
+            // Set cache options
+            var cacheOptions = new MemoryCacheEntryOptions()
+                .SetSlidingExpiration(TimeSpan.FromSeconds(30));
+
+        _memoryCache.Set(weatherForecastKey, weatherForecastCollection, cacheOptions);
+            return weatherForecastCollection;
+        }
+
+        private static IEnumerable<WeatherForecast> GetWeatherForecast()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
